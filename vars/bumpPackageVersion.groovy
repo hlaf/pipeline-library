63b228853ee1ea23eb8eebe38ee242df36d0230c
hlaf <henriquelindgren@gmail.com>
2018-09-09 16:53:37 +0100

    Add the bumpPackageVersion step.

---
+++ vars/bumpPackageVersion.groovy
@@ -0,0 +1,32 @@
+def call(String git_repo_creds,
+         String author,
+         String author_email) {
+    
+    tag_name = 'latest'
+  
+    sshagent([git_repo_creds]) {
+        sh """
+            version_file="src/${env.JOB_NAME}/__init__.py"
+            current_version=\$(grep __version__ \$version_file | awk -F\\   '{print \$3 }' | sed "s/'//g")
+            source master_venv/bin/activate
+            \\pip install semver --upgrade
+            new_version=\$(python -c "import semver; print semver.bump_patch(\'\${current_version}\')")
+        
+            # Update the __init__.py file with the new version.
+            sed -i s/\$current_version/\$new_version/ \$version_file
+            
+            git commit --author '${author} <${author_email}>' -m "Bump version to \${new_version}." \$version_file
+            git push --set-upstream origin master
+            
+            git tag -fa \$new_version -m \"Create tag for version \${new_version}.\"
+            git tag -fa \"${tag_name}\" -m \"Update the '${tag_name}' tag.\"
+               
+            # delete tag on remote in order not to fail pushing the new one
+            git push origin :refs/tags/${tag_name}
+           
+            # push the tags
+            git push -f origin refs/tags/${tag_name}:refs/tags/${tag_name}
+            git push -f origin refs/tags/\${new_version}:refs/tags/\${new_version}
+        """
+    }
+}
