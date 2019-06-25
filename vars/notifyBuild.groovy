8a7df323bf8a39b625b7c43d7360c2cffef29ad1
hlaf <henriquelindgren@gmail.com>
2019-06-25 01:28:19 +0100

    Create the notifyBuild step

---
+++ vars/notifyBuild.groovy
@@ -0,0 +1,12 @@
+def call(String build_status,
+         String recipient_email) {
+
+  emailext (
+	 to: recipient_email,
+	 subject: "${build_status}: ${env.JOB_NAME} - Build # ${env.BUILD_NUMBER}",
+	 body: """${env.JOB_NAME} - Build # ${env.BUILD_NUMBER}
+
+Check console output at ${env.BUILD_URL} to view the results.""",
+  )
+
+}
