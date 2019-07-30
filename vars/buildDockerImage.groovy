fc2371ff503d21b9df7269c20a89f4b7dfeb9deb
hlaf <henriquelindgren@gmail.com>
2019-07-30 19:06:38 +0100

    Add the 'environment' parameter buildDockerImage

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,6 +1,7 @@
 def call(String image_name,
 	     String domain_name,
-		 String manager_node) {
+		 String manager_node,
+		 String environment='production') {
 		 
 	node('docker-slave') {
 		if (dockerImageExists(image_name)) {
@@ -11,7 +12,7 @@ def call(String image_name,
 		echo "Building image ${image_name}"
 		image_fqdn = image_name + '.dockerbuilder.' + domain_name
 	
-		createPuppetDockerfile(image_name)
+		createPuppetDockerfile(image_name, environment: environment)
 		deletePuppetCertificate(image_fqdn, manager_node)
 		image = docker.build(image_name)
 
