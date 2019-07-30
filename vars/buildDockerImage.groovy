a649731194adfba989530cc14febd0aa2a2eab20
hlaf <henriquelindgren@gmail.com>
2019-07-30 19:14:39 +0100

    Fix syntax

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -12,7 +12,7 @@ def call(String image_name,
 		echo "Building image ${image_name}"
 		image_fqdn = image_name + '.dockerbuilder.' + domain_name
 	
-		createPuppetDockerfile(image_name, environment: environment)
+		createPuppetDockerfile(image_name, environment)
 		deletePuppetCertificate(image_fqdn, manager_node)
 		image = docker.build(image_name)
 
