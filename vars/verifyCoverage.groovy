1e4d5f1dcfb61cfeae4eb7986e2f89747a424f09
hlaf <henriquelindgren@gmail.com>
2019-08-14 23:45:53 +0100

    Use a sensible default baseline coverage value for new projects

--- vars/verifyCoverage.groovy
+++ vars/verifyCoverage.groovy
@@ -2,41 +2,48 @@ def call() {
     
     // Copy the latest successful build's coverage.xml
 	def previous_build = getLatestSuccessfulBuildWithArtifacts()
+	def default_baseline_coverage = '0.85'
+	def branch_rate_old
+	def line_rate_old
 	
-	try {
+	if (previous_build == null) {
+		echo "Couldn't find prior coverage results - using the default baseline for new projects"
+
+		branch_rate_old = new BigDecimal(default_baseline_coverage).setScale(2, java.math.RoundingMode.HALF_UP)
+		line_rate_old = new BigDecimal(default_baseline_coverage).setScale(2, java.math.RoundingMode.HALF_UP)
+
+	} else {
 		copyArtifacts(projectName: "${env.JOB_NAME}",
-			          selector: specific("${previous_build.number}"),
-					  target: 'previous_build', filter: '**/coverage.xml',
+				      selector: specific("${previous_build.number}"),
+			          target: 'previous_build', filter: '**/coverage.xml',
 			          flatten: true);
-	} catch (Exception e) {
-		if (e.message.contains('Failed to copy artifacts')) {
-			echo "Couldn't find prior coverage results - assuming new project"
-			return
-		}
-		throw e
+
+		def xml_old = readFile "${env.WORKSPACE}/previous_build/coverage.xml"
+		def coverage_old = new XmlParser().parseText(xml_old)
+		branch_rate_old = new BigDecimal(coverage_old['@branch-rate']).setScale(2, java.math.RoundingMode.HALF_UP)
+		line_rate_old = new BigDecimal(coverage_old['@line-rate']).setScale(2, java.math.RoundingMode.HALF_UP)
 	}
-    
-    def xml_old = readFile "${env.WORKSPACE}/previous_build/coverage.xml"
-    def coverage_old = new XmlParser().parseText(xml_old)
-    def branch_rate_old = new BigDecimal(coverage_old['@branch-rate']).setScale(2, java.math.RoundingMode.HALF_UP)
-    def line_rate_old = new BigDecimal(coverage_old['@line-rate']).setScale(2, java.math.RoundingMode.HALF_UP)
-    
+
     def xml_new = readFile "${env.WORKSPACE}/coverage.xml"
     def coverage_new = new XmlParser().parseText(xml_new)
     def branch_rate_new = new BigDecimal(coverage_new['@branch-rate']).setScale(2, java.math.RoundingMode.HALF_UP)
     def line_rate_new = new BigDecimal(coverage_new['@line-rate']).setScale(2, java.math.RoundingMode.HALF_UP)
     
-	echo "Current coverage metrics:"
-	echo "  Branch rate: ${coverage_old['@branch-rate']}"
-	echo "  Line rate  : ${coverage_old['@line-rate']}"
+	echo "Baseline coverage metrics:"
+	echo "  Branch rate: ${branch_rate_old}"
+	echo "  Line rate  : ${line_rate_old}"
 	
-	echo "New build's coverage metrics:"
-	echo "  Branch rate: ${coverage_new['@branch-rate']}"
-	echo "  Line rate  : ${coverage_new['@line-rate']}"
+	echo "Current build's coverage metrics:"
+	echo "  Branch rate: ${branch_rate_new}"
+	echo "  Line rate  : ${line_rate_new}"
 
 	// Fail the job if coverage decreases
 	if (branch_rate_new < branch_rate_old || line_rate_new < line_rate_old) {
-		error "Code coverage decreased from build ${previous_build.number} to build ${env.BUILD_NUMBER}"
+		if (previous_build != null) {
+			error "Code coverage decreased from build ${previous_build.number} to build ${env.BUILD_NUMBER}"
+		} else {
+			error "Code coverage is too low"
+		}
 	}
 
 }
