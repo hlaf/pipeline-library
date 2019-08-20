dd3de708b9aa86a898327df364dbdb611212da14
hlaf <henriquelindgren@gmail.com>
2019-08-20 15:02:29 +0100

    Add Windows support

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -4,14 +4,24 @@ def call(String environment = '') {
 	}
 
 	try {
-		sh """
-	        source master_venv/bin/activate
-	
-	        # NOTE: Ignore aliases
-	        \\pip install tox --upgrade
-	
-	        tox ${environment}
-	    """
+		if (System.properties['os.name'].toLowerCase().contains('windows')) {
+			bat """
+			master_venv/Scripts/activate.bat
+			pip install tox --upgrade
+			tox ${environment}
+			"""
+		} else {
+			sh """
+			source master_venv/bin/activate
+			
+			# NOTE: Ignore aliases
+			\\pip install tox --upgrade
+			
+			tox ${environment}
+			"""
+		}
+		
+		
 	} finally {
 		publishJUnitReport()
 	}
