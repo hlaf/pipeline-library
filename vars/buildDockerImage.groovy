6173a2692989d488209961d1816d51d59da2671f
hlaf <henriquelindgren@gmail.com>
2019-12-02 18:11:25 +0000

    Trying to realize where 'dnsdomainname' should be called from

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,6 +2,8 @@ def call(String image_name,
 		 String manager_node,
 		 String environment='dockerbuilder') {
 		 
+	domain_name = sh(script: 'dnsdomainname', returnStdout: true).trim()
+
 	node('docker-slave') {
 		if (dockerImageExists(image_name)) {
 			echo "The image ${image_name} already exists. Skipping build."
@@ -10,7 +12,6 @@ def call(String image_name,
 
 		echo "Building image ${image_name}"
 		
-		domain_name = sh(script: 'dnsdomainname', returnStdout: true).trim()
 		image_fqdn = image_name + '.dockerbuilder.' + domain_name
 	
 		createPuppetDockerfile(image_name, environment)
