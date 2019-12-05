cc131fc7bf3beac9a4d6b589b7cba15caf075fd7
hlaf <henriquelindgren@gmail.com>
2019-12-05 17:27:41 +0000

    Workaround for publishing multiple coverage reports

--- vars/publishCoberturaReport.groovy
+++ vars/publishCoberturaReport.groovy
@@ -1,5 +1,15 @@
-def call() {
-    // Publish the Cobertura test coverage report
-    step([$class: 'CoberturaPublisher', coberturaReportFile: '**/coverage.xml'])
-    archiveArtifacts artifacts: '**/coverage.xml', fingerprint: true
+def call(results=[]) {
+    // Publish the Cobertura test coverage report(s)
+	def report_location
+	if (results.size() > 0) {
+	    for (String k: results) {
+		    dir("./coverage_results/${k}") { unstash k }
+	    }
+		report_location = 'coverage_results/**/coverage.xml'
+	} else {
+		report_location = '**/coverage.xml'
+		archiveArtifacts artifacts: report_location, fingerprint: true
+	}
+
+	step([$class: 'CoberturaPublisher', coberturaReportFile: report_location])
 }
