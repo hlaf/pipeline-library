74d44b62692caa7a085200fc8fd1c89a2b299e23
hlaf <henriquelindgren@gmail.com>
2019-12-05 19:34:01 +0000

    Add the uploadToArtifactRepository() step

---
+++ vars/uploadToArtifactRepository.groovy
@@ -0,0 +1,16 @@
+def call(String artifact_repo_url,
+	     String artifact_repo_creds) {
+
+    withCredentials([usernamePassword(credentialsId: artifact_repo_creds,
+		        	 usernameVariable: 'TWINE_USERNAME',
+			         passwordVariable: 'TWINE_PASSWORD')]) {
+		sh """
+          virtualenv master_venv
+          . master_venv/bin/activate
+          python setup.py sdist
+          \\pip install twine --upgrade
+          twine upload --repository-url ${artifact_repo_url} dist/*
+		"""
+	}
+
+}
