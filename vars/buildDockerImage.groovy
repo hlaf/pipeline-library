cc61e61b8e0818e3b322006884436c75a2223efb
hlaf <henriquelindgren@gmail.com>
2019-12-17 00:20:14 +0000

    Set a default value for 'manager_node'

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,7 +2,7 @@ def call(Map parameters=[:]) {
 
 	String image_name = parameters.image_name
 	String image_user = parameters.image_user ?: 'root'
-	String manager_node = parameters.manager_node
+	String manager_node = parameters.manager_node ?: 'puppet_management_node'
 	String environment = parameters.environment ?: 'dockerbuilder'
 	String from_image_name = parameters.from_image_name ?: 'hlaf/puppet'
 	boolean force = parameters.force ?: false
