3318076835a7b3830e7ffcbe48acf4293461212c
hlaf <henriquelindgren@gmail.com>
2019-12-30 02:51:11 +0000

    JPL-1: Add sanity checks to the stash/unstashCoverageResult steps

--- vars/stashCoverageResult.groovy
+++ vars/stashCoverageResult.groovy
@@ -1,5 +1,11 @@
 def call(Map parameters=[:]) {
   String key = parameters.key
   
-  stash name: "coverage-${key}", includes: "coverage.xml"
+  stash_name = "coverage-${key}"
+  
+  if (stashExists(stash_name)) {
+    error "The stash '${stash_name}' already exists"
+  }
+
+  stash name: stash_name, includes: "coverage.xml"
 }
--- vars/unstashCoverageResult.groovy
+++ vars/unstashCoverageResult.groovy
@@ -2,6 +2,11 @@ def call(Map parameters=[:]) {
   String key = parameters.key
   
   String unstash_path = "./coverage_results/${key}"
+  
+  if (fileExists(unstash_path)) {
+    error "Unstash location already exists: '${unstash_path}'"
+  }
+  
   dir(unstash_path) { unstash "coverage-${key}" }
   return unstash_path
 }
