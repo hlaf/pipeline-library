f41c643a4725eaeca1b52f54531c0fb33b71ae56
hlaf <henriquelindgren@gmail.com>
2019-12-30 17:45:13 +0000

    JPL-1: Move stash sanity checks to the saferStash step

--- vars/stashCoverageResult.groovy
+++ vars/stashCoverageResult.groovy
@@ -1,11 +1,5 @@
 def call(Map parameters=[:]) {
   String key = parameters.key
-  
   stash_name = "coverage-${key}"
-  
-  if (stashExists(stash_name)) {
-    error "The stash '${stash_name}' already exists"
-  }
-
-  stash name: stash_name, includes: "coverage.xml"
+  saferStash name: stash_name, includes: "coverage.xml"
 }
