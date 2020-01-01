aafa4e4e60bc7cce4912680e3409c2d065387492
hlaf <henriquelindgren@gmail.com>
2020-01-01 22:48:34 +0000

    BU-4: Implement the getPipelineConfig() helper

---
+++ vars/getPipelineConfig.groovy
@@ -0,0 +1,27 @@
+def call() {
+
+	return PipelineConfigManager.inst().get("${env.JOB_NAME}@${env.BUILD_NUMBER}")
+  
+}
+
+protected class PipelineConfigManager {
+	
+	private final Map _items;
+	private static PipelineConfigManager _inst;
+
+	private PipelineConfigManager() {
+		_items = [:].withDefault { k -> [:] };
+	}
+
+	protected synchronized static PipelineConfigManager inst() {
+		if (_inst == null) {
+			_inst = new PipelineConfigManager();
+		}
+		return _inst;
+	}
+	
+	protected get(String config_name) {
+		return _items.get(config_name)
+	}
+
+}
