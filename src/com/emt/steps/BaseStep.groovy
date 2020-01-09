f4cad1dc65c6bb085b38ea1e38bcc7b636b7919a
hlaf <henriquelindgren@gmail.com>
2020-01-09 19:44:47 +0000

    Try to disable the default step constructor (at compile time)

--- src/com/emt/steps/BaseStep.groovy
+++ src/com/emt/steps/BaseStep.groovy
@@ -1,12 +1,18 @@
 package com.emt.steps
 
+import org.jenkinsci.plugins.workflow.cps.CpsScript
+
+import com.emt.IStepExecutor
+
 abstract class BaseStep implements Serializable {
 
 	protected Object _steps;
 	
-	private BaseStep() {}
-
-	BaseStep(Object executor) {
+	BaseStep(CpsScript executor) {
+		_steps = executor
+	}
+	
+	BaseStep(IStepExecutor executor) {
 		_steps = executor
 	}
 
