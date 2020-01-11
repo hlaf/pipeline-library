f6c14cf9a2f12e79ce8485d9e283c5c37b740b67
hlaf <henriquelindgren@gmail.com>
2020-01-11 22:11:21 +0000

    JPL-18: Add missing constructor argument

--- vars/bumpPackageVersion.groovy
+++ vars/bumpPackageVersion.groovy
@@ -7,8 +7,8 @@ def call(String git_repo_creds,
          String author_email,
 		 String version_file = "src/${env.JOB_NAME}/__init__.py") {
     
-	new BumpPackageVersion().execute(git_repo_creds: git_repo_creds,
-								     author: author,
-									 author_email: author_email,
-									 version_file: version_file)
+	new BumpPackageVersion(this).execute(git_repo_creds: git_repo_creds,
+									     author: author,
+										 author_email: author_email,
+										 version_file: version_file)
 }
