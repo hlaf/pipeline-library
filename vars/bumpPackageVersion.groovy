0bf875c595a35e6116925d839852cedec890c204
hlaf <henriquelindgren@gmail.com>
2020-03-04 16:48:47 +0000

    INFRA-48: Add the 'version_key' parameter to bumpPackageVersion()

--- vars/bumpPackageVersion.groovy
+++ vars/bumpPackageVersion.groovy
@@ -5,10 +5,12 @@ import com.emt.steps.BumpPackageVersion
 def call(String git_repo_creds,
          String author,
          String author_email,
-		 String version_file = "src/${env.JOB_NAME}/__init__.py") {
+		 String version_file = "src/${env.JOB_NAME}/__init__.py",
+		 String version_key = "__version__") {
     
 	new BumpPackageVersion(this).execute(git_repo_creds: git_repo_creds,
 									     author: author,
 										 author_email: author_email,
-										 version_file: version_file)
+										 version_file: version_file,
+										 version_key: version_key)
 }
