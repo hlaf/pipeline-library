40ef359f3b56ef6205a64517daf6f99eb4aebe2b
hlaf <henriquelindgren@gmail.com>
2020-03-05 15:41:35 +0000

    Revert "Revert "Revert "INFRA-48: Pass the branch explicitly"""
    This reverts commit 92929590431f7451f1e6fafa06b258ba34a2d557.

--- vars/bumpPackageVersion.groovy
+++ vars/bumpPackageVersion.groovy
@@ -6,13 +6,11 @@ def call(String git_repo_creds,
          String author,
          String author_email,
 		 String version_file = "src/${env.JOB_NAME}/__init__.py",
-		 String version_key = "__version__",
-		 String branch = "master") {
+		 String version_key = "__version__") {
     
 	new BumpPackageVersion(this).execute(git_repo_creds: git_repo_creds,
 									     author: author,
 										 author_email: author_email,
 										 version_file: version_file,
-										 version_key: version_key,
-										 branch: branch)
+										 version_key: version_key)
 }
