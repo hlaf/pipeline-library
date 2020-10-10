5336ee560a3ed72e7613a8c810a61584b56bb875
hlaf <henriquelindgren@gmail.com>
2020-10-10 20:32:42 +0100

    JPL-39: Enable configuration inheritance

--- src/com/emt/steps/DeletePuppetCertificate.groovy
+++ src/com/emt/steps/DeletePuppetCertificate.groovy
@@ -4,18 +4,18 @@ package com.emt.steps
 class DeletePuppetCertificate extends BaseStep {
 	@Override
 	Object execute(Map params=[:]) {
+        Map config = MapUtils.merge(_steps.getPipelineConfig(), params)
 		
-		String certificate_name = params.certificate_name
-		String manager_node = params.get('manager_node', 'puppet_management_node')
-		String master = params.get('master', 'puppet')
-		String environment = params.get('environment', 'production')
-        String ssl_cert_path = params.get('ssl_cert_path',
+		String certificate_name = config.certificate_name
+		String manager_node = config.get('manager_node', 'puppet_management_node')
+		String master = config.get('master', 'puppet')
+		String environment = config.get('environment', 'production')
+        String ssl_cert_path = config.get('ssl_cert_path',
                                           "/var/lib/puppet/ssl/certs/\${HOSTNAME}.pem")
-        String ssl_cert_key = params.get('ssl_cert_key',
+        String ssl_cert_key = config.get('ssl_cert_key',
                                          "/var/lib/puppet/ssl/private_keys/\${HOSTNAME}.pem")
 
         _steps.node(manager_node) {
-			
 			_steps.sh """
 			curl -sS -f --insecure --cert ${ssl_cert_path} --key ${ssl_cert_key} \
 			     -d '{\"desired_state\":\"revoked\"}' -H 'Content-Type: text/pson' \
