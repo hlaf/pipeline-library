218b182a54ed73c6d9c04f7f17f6635569ee92c2
hlaf <henriquelindgren@gmail.com>
2021-03-01 18:12:42 +0000

    INFRA-589: Add the test_results parameter to the PublishJUnitReport step

--- src/com/emt/steps/PublishJUnitReport.groovy
+++ src/com/emt/steps/PublishJUnitReport.groovy
@@ -1,8 +1,14 @@
 package com.emt.steps
 
+import com.emt.common.MapUtils
+
 @groovy.transform.InheritConstructors
 class PublishJUnitReport extends BaseStep {
-	Object execute(Map parameters=[:]) {
-		_steps.junit keepLongStdio: true, testResults: '**/test_results.xml'
+	Object execute(Map params=[:]) {
+        
+        Map config = MapUtils.merge(_steps.getPipelineConfig(), params)
+        
+        String test_results = config.get('test_results', '**/test_results.xml')
+		_steps.junit keepLongStdio: true, testResults: test_results
 	}
 }
