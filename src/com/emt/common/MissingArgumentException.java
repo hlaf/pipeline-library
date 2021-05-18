ccd962f10d85f6160482afda23229085d227b06b
Henrique Lindgren <henriquelindgren@gmail.com>
2021-05-18 20:04:57 +0100

    Revert "JPL-49: Make class serializable to (hopefully) fix CPS errors "
    This reverts commit d420101abefd4175284dc14677e19dfc8dc4a8dc.

--- src/com/emt/common/MissingArgumentException.java
+++ src/com/emt/common/MissingArgumentException.java
@@ -1,10 +1,6 @@
 package com.emt.common;
 
-import java.io.Serializable;
-
-public class MissingArgumentException extends Exception implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+public class MissingArgumentException extends Exception {
     public MissingArgumentException(String message) {
         super(message);
     }
