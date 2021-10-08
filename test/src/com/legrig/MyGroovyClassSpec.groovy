41940f64e8ca8915811e4d480454f16efe91fa05
Henrique Lindgren <henriquelindgren@gmail.com>
2021-10-08 20:14:32 +0100

    JPL-57: Add safeguard to confirm that code under test is CPS transformed

--- test/src/com/legrig/MyGroovyClassSpec.groovy
+++ test/src/com/legrig/MyGroovyClassSpec.groovy
@@ -1,8 +1,10 @@
 package com.legrig
 
 import com.cloudbees.groovy.cps.impl.CpsCallableInvocation
-import support.jenkins.CPSSpecification
+
+import spock.lang.IgnoreIf
 import spock.lang.Unroll
+import support.jenkins.CPSSpecification
 
 class MyGroovyClassSpec extends CPSSpecification {
 
@@ -21,30 +23,32 @@ class MyGroovyClassSpec extends CPSSpecification {
       2 | 3 || 6
   }
 
-  //def "MyGroovyClass.multiply() will not run without CPS Test"() {
-  //  when: 'Data to be correct'
-  //    sut.multiply(1, 2)
+  @IgnoreIf({ Boolean.getBoolean('com.emt.use_cps') == false })
+  def "MyGroovyClass.multiply() will not run without CPS Test"() {
+    when: 'Data to be correct'
+      sut.multiply(1, 2)
 
-  //  then: 'CpsCallableInvocation Exception is thrown'
-  //    thrown(CpsCallableInvocation)
-  //}
+    then: 'CpsCallableInvocation Exception is thrown'
+      thrown(CpsCallableInvocation)
+  }
 
-  //@Unroll
-  //def "MyGroovyClass.multiply(#x, #y)== #expected under CPS Test"() {
+  @IgnoreIf({ Boolean.getBoolean('com.emt.use_cps') == false })
+  @Unroll
+  def "MyGroovyClass.multiply(#x, #y)== #expected under CPS Test"() {
 
-  //  when: 'we invoke multiply() via CPS'
-  //    def result = invokeCPSMethod(sut, 'multiply', x, y)
+    when: 'we invoke multiply() via CPS'
+      def result = invokeCPSMethod(sut, 'multiply', x, y)
 
-  //  then: 'Exception is not thrown'
-  //    notThrown(CpsCallableInvocation)
+    then: 'Exception is not thrown'
+      notThrown(CpsCallableInvocation)
 
-  //  and: 'result is correct'
-  //    result == expected
+    and: 'result is correct'
+      result == expected
 
-  //  where: 'data is'
-  //    x | y || expected
-  //    1 | 1 || 1
-  //    1 | 0 || 0
-  //    2 | 3 || 6
-  //}
+    where: 'data is'
+      x | y || expected
+      1 | 1 || 1
+      1 | 0 || 0
+      2 | 3 || 6
+  }
 }
