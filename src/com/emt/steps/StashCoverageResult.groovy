09aa06f11c944e06d21fc373dee87261f9801456
Henrique Lindgren <henriquelindgren@gmail.com>
2022-01-11 23:16:03 +0000

    PUPINF-59: Use consistent default values for the same parameter type

--- src/com/emt/steps/StashCoverageResult.groovy
+++ src/com/emt/steps/StashCoverageResult.groovy
@@ -1,10 +1,12 @@
 package com.emt.steps
 
+import static com.emt.steps.UnstashCoverageResult.COVERAGE_STASH_NAME_PREFIX;
+
 @groovy.transform.InheritConstructors
 class StashCoverageResult extends BaseStep {
 	Object execute(Map parameters=[:]) {
 		String key = parameters.key
-		String stash_name = "coverage-${key}"
+		String stash_name = COVERAGE_STASH_NAME_PREFIX + key
 		_steps.saferStash name: stash_name, includes: "coverage.xml"
 	}
 }
--- src/com/emt/steps/UnstashCoverageResult.groovy
+++ src/com/emt/steps/UnstashCoverageResult.groovy
@@ -4,12 +4,13 @@ package com.emt.steps
 class UnstashCoverageResult extends BaseStep {
 	
 	public static final String COVERAGE_RESULTS_BASE_DIR = "coverage_results";
+    public static final String COVERAGE_STASH_NAME_PREFIX = "coverage-";
 	
 	Object execute(Map parameters=[:]) {
 		String key = parameters.key
 
 		String unstash_path = "./${COVERAGE_RESULTS_BASE_DIR}/${key}"
-		_steps.saferUnstash key: "coverage-${key}", to: unstash_path
+		_steps.saferUnstash key: COVERAGE_STASH_NAME_PREFIX + key, to: unstash_path
 
 		return unstash_path
 	}
