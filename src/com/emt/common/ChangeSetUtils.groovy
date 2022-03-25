fa17e7dfbb28bff7e8e7cc482978f873a3a8eaf9
Henrique Lindgren <henriquelindgren@gmail.com>
2022-03-25 13:28:16 +0000

    INFRA-1205: Add debug-level logging statements to ChangeSetUtils

--- src/com/emt/common/ChangeSetUtils.groovy
+++ src/com/emt/common/ChangeSetUtils.groovy
@@ -1,8 +1,7 @@
 package com.emt.common;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger
+import java.util.stream.StreamSupport
 
 import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
 
@@ -12,25 +11,30 @@ import hudson.scm.ChangeLogSet.Entry;
 
 class ChangeSetUtils implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
+    private static final transient Logger logger = Logger.getLogger("com.emt.common.ChangeSetUtils");
 
-    @CoverageIgnoreGenerated
     static List<AffectedFile> getChangedFiles(RunWrapper build) {
-        List<ChangeLogSet<? extends Entry>> changeLogSets;
-        try {
-            changeLogSets = build.getChangeSets();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        List<AffectedFile> res = new ArrayList();
+        List<ChangeLogSet<? extends Entry>> changeLogSets = build.getChangeSets();
+        logger.fine("Found " + changeLogSets.size() + " change sets");
+        List<AffectedFile> changed_files = new ArrayList();
 
         for (ChangeLogSet change_set: changeLogSets) {
+            int n_entries = 0;
+            for (Object entry: change_set) ++n_entries;
+            logger.fine("The change set contains " + n_entries + " entries.");
             for (Object entry: change_set) {
-                res.addAll(((ChangeLogSet.Entry)entry).getAffectedFiles());
+                List<AffectedFile> tmp = new ArrayList();
+                tmp.addAll(((ChangeLogSet.Entry)entry).getAffectedFiles());
+                logger.fine("The entry contains " + tmp.size() + " changed files");
+                for (AffectedFile f: tmp) {
+                    logger.fine("Affected file: " + f.getPath());
+                    changed_files.add(f);
+                }
             }
         }
 
-        return res;
+        return changed_files;
     }
 
 }
--- src/com/emt/steps/FileChanged.groovy
+++ src/com/emt/steps/FileChanged.groovy
@@ -1,7 +1,14 @@
 package com.emt.steps
 
-import groovy.transform.InheritConstructors
+import java.util.logging.Handler
+import java.util.logging.Level
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
+
 import com.emt.common.ChangeSetUtils
+import com.emt.common.CustomHandler
+
+import groovy.transform.InheritConstructors
 
 @groovy.transform.InheritConstructors
 class FileChanged extends BaseStep {
@@ -11,6 +18,20 @@ class FileChanged extends BaseStep {
     Object execute(Map parameters=[:]) {
         validateParameters(parameters);
         String name = parameters.name
+        //def handler = new CustomHandler(_steps);
+        //handler.setLevel(Level.ALL);
+        //handler.setFormatter(new SimpleFormatter());
+
+        //String logger_name = "com.emt.common"
+
+        //Handler[] handlers = Logger.getLogger(logger_name).getHandlers();
+        //for (Handler h: handlers) {
+        //    if (h instanceof CustomHandler) {
+        //        Logger.getLogger(logger_name).removeHandler(h);
+        //    }
+        //}
+        //handlers = null;
+        //Logger.getLogger(logger_name).addHandler(handler);
 
         if (!_steps.fileExists(name)) {
             return error_helper("The file '${name}' does not exist.")
